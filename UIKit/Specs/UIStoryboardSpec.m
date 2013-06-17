45d33304f0a9d0467a34bfb2ac3895b4f04d5350
Jason Allum <jason@dmgctrl.com>
2013-06-17 10:51:47 -0400

    Removed unused variable.

--- UIKit/Specs/UIStoryboardSpec.m
+++ UIKit/Specs/UIStoryboardSpec.m
@@ -6,7 +6,7 @@ describe(@"UIStoryboard", ^{
     context(@"+storyboardWithName:bundle:", ^{
         it(@"should throw an assertion failure when passed a nil name", ^{
             @try {
-                UIStoryboard* storyboard = [UIStoryboard storyboardWithName:nil bundle:nil];
+                [UIStoryboard storyboardWithName:nil bundle:nil];
                 fail(@"Didn't throw an exception");
             } @catch (id x) {
                 [[[x description] should] equal:@"Invalid parameter not satisfying: [name length] > 0"];
